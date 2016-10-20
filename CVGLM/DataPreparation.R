e77f4106c40f7aca4697dd9992011c12626e0a7a
artur-m <arturmenc@gmail.com>
2016-10-21 00:03:41 +0200

    CV

--- CVGLM/DataPreparation.R
+++ CVGLM/DataPreparation.R
@@ -13,7 +13,8 @@
 
 library(data.table)
 library(dplyr)
-
+library(caret)
+library(ROCR)
 
 source('CVGLM/ToolFunctionWS.R')
 
@@ -79,6 +80,80 @@ set.categorization.and.woe.data.frame <- function( data, features, f.trans, flag
 }
 
 
+### funkcja ktora zadany zbior dzieli na k-fold, nastepnie 
+
+build.cv.dataset <- function(dataset,features,target,k,seed)
+{
+  set.seed(seed)
+  train.folds <- caret::createFolds(dataset[,target],k = 5,returnTrain = TRUE)
+  
+  train.cv <- lapply(train.folds, FUN = function(x){
+    dataset[x,c(features,target)]
+  })
+  test.cv <-  lapply(train.folds, FUN = function(x){
+    dataset[-x,c(features,target)]
+  }) 
+  list(train.cv = train.cv,test.cv = test.cv)
+}
+
+categorize.and.woe.cv <- function( train.cv,features,target,min.levels.to.continuous = 6,eps =0.1  )
+{
+  
+  dict.cv <-lapply(train.cv, FUN = function(data){
+    categorize.and.woe.data.frame(data= data,features = features,target = target, min.levels.to.continuous = min.levels.to.continuous, eps = eps)
+  })
+  
+  
+}
+
+
+set.categorization.and.woe.cv <- function(cv.dataset, cv.trans, features)
+{
+  k <- length(cv.dataset$train.cv)
+  trans.train.cv <- lapply(1:k,FUN = function(i)
+    {
+    set.categorization.and.woe.data.frame( data = cv.dataset$train.cv[[i]],features = features,f.trans = cv.trans[[i]] )
+  })
+  trans.test.cv <- lapply(1:k,FUN = function(i)
+  {
+    set.categorization.and.woe.data.frame( data = cv.dataset$test.cv[[i]],features = features,f.trans = cv.trans[[i]] )
+  })
+  list(trans.train.cv = trans.train.cv,trans.test.cv=trans.test.cv)
+}
+
+
+build.glm.model.and.predict.cv <- function( trans.cv.dataset, features, target,k)
+{
+  formula <-as.formula(paste(target,'~',paste(features,collapse = '+')))
+  
+  formula
+  lapply(1:k, FUN = function(i)
+         {
+    model <- glm(formula,data =  trans.cv.dataset$trans.train.cv[[i]],family = 'binomial')
+    predict.trn <- predict(model,trans.cv.dataset$trans.train.cv[[i]])
+    predict.tst <- predict(model,trans.cv.dataset$trans.test.cv[[i]])
+    list(predict.trn = predict.trn,y.trn =trans.cv.dataset$trans.train.cv[[i]][,target] 
+         ,predict.tst=predict.tst,y.tst = trans.cv.dataset$trans.test.cv[[i]][,target])
+  })
+
+}
+
+
+auc.evaluate <- function(predicted,observed)
+{
+  pred <- ROCR::prediction(predicted,observed)
+  perf <- ROCR::performance(pred,"tpr","fpr")
+  perf <- ROCR::performance(pred,"auc")
+  as.numeric(perf@y.values)
+}
+
+
+auc.evaluate.cv <- function(pred.cv)
+{
+  sapply(pred.cv, FUN = function(pred){
+    auc.evaluate(pred$predict.tst,pred$y.tst)
+  })
+}
 
 
 
@@ -90,7 +165,23 @@ data <- iris[,1:4]
 data$target <- ifelse(iris$Species =='virginica',1,0)
 data$target <- sample(c(1,0),150,r=T)
 
-trans.df <- categorize.and.woe.data.frame(data,names(data)[-5],'target',eps = 0.00000000001)
+features <- names(data)[-5]
+target <- 'target'
+trans.df <- categorize.and.woe.data.frame(data,features,'target',eps = 0.1)
 
 set.categorization.and.woe.data.frame(data = data, features = names(data)[1:2],f.trans = trans.df)
 
+
+####### TUTAJ MIESO 
+
+cv.dataset <- build.cv.dataset(dataset = data,features = features,target = 'target',k = 5 ,seed = 112)
+
+cv.trans <- categorize.and.woe.cv(train.cv = cv.dataset$train.cv,features = features, target = target)
+
+trans.cv.dataset <- set.categorization.and.woe.cv(cv.dataset,cv.trans,features = features)
+
+pred.cv <- build.glm.model.and.predict.cv(trans.cv.dataset = trans.cv.dataset,features = features[1:2],k = 5,target = 'target')
+
+set.auc.tst <- auc.evaluate.cv(pred.cv)
+mean(set.auc.tst)
+sd(set.auc.tst)
