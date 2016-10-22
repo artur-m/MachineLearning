e8aad45affa66ba9a77e27479c3d738e00554747
artur-m <arturmenc@gmail.com>
2016-10-22 15:32:01 +0200

    stepAUC

--- CVGLM/DataPreparation.R
+++ CVGLM/DataPreparation.R
@@ -85,7 +85,7 @@ set.categorization.and.woe.data.frame <- function( data, features, f.trans, flag
 build.cv.dataset <- function(dataset,features,target,k,seed)
 {
   set.seed(seed)
-  train.folds <- caret::createFolds(dataset[,target],k = 5,returnTrain = TRUE)
+  train.folds <- caret::createFolds(dataset[,target],k = k,returnTrain = TRUE)
   
   train.cv <- lapply(train.folds, FUN = function(x){
     dataset[x,c(features,target)]
@@ -150,14 +150,95 @@ auc.evaluate <- function(predicted,observed)
 
 auc.evaluate.cv <- function(pred.cv)
 {
-  sapply(pred.cv, FUN = function(pred){
+  trn.set <- sapply(pred.cv, FUN = function(pred){
+    auc.evaluate(pred$predict.trn,pred$y.trn)
+  })
+  
+  tst.set <- sapply(pred.cv, FUN = function(pred){
     auc.evaluate(pred$predict.tst,pred$y.tst)
   })
+  list(trn.set = trn.set, tst.set = tst.set)
 }
 
 
+step.auc.cv <- function( cv.dataset, features, target, steps, k)
+{
+  selected.features <- c()
+  current.selected.features <- c()
+  avaiable.features <- features
+  formula <- as.formula(paste(target,'~.'))
+  best.auc  <- random.auc.set.cv(k)
+  best.s.auc <- random.auc.set.cv(k)
+  s <- 1
+  while( s < steps)
+  {
+    best.s.auc <- random.auc.set.cv(k)
+    best.s.feature <- -1
+    for( f in avaiable.features)
+    {
+      current.selected.features <- c(selected.features,f)
+      pred.cv <- build.glm.model.and.predict.cv(trans.cv.dataset = cv.dataset
+                                                ,features = current.selected.features
+                                                ,target = target,k = k)
+      
+      current.auc.set <- auc.evaluate.cv(pred.cv)
+      if( test.predict.ability.cv( current.auc.set) )
+      {
+        if( auc.model.cmp.cv(best.s.auc, current.auc.set)  )
+        {
+          best.s.auc = current.auc.set
+          best.s.feature <- f
+        }
+        
+      }
+      
+      
+    }
+    print(best.s.feature)
+    if( auc.model.cmp.cv(best.auc, best.s.auc))
+    {
+      avaiable.features <- avaiable.features[best.s.feature!=avaiable.features]
+      selected.features <- c(selected.features,best.s.feature)
+      s <- s+1
+      
+      print(avaiable.features)
+      print(selected.features)
+      print(best.s.auc)
+      
+    }
+    else
+    {
+      break;
+    }
 
+    
+    
+  }
+  list(selected.features, best.auc)
+}
 
+test.predict.ability.cv <- function( set.auc.cv)
+{
+  if( min( abs(set.auc.cv$trn.set - set.auc.cv$tst.set) ) > 0.1 ) return(FALSE)
+  sd.tst <- sd(set.auc.cv$tst)
+  sd.trn <- sd(set.auc.cv$trn)
+  if( sd.tst > 0.1 || sd.trn > 0.1 ) return(FALSE)
+  if( any( set.auc.cv$tst.set - sd.tst < 0.5 ) || any( set.auc.cv$trn.set - sd.trn < 0.5 ) ) return(FALSE)
+  return(TRUE)
+}
+
+### x < y
+auc.model.cmp.cv <- function( set.auc.cv.x, set.auc.cv.y)
+{
+  mean(set.auc.cv.x$tst.set) < mean(set.auc.cv.y$tst.set)
+}
+
+random.auc.set.cv <- function(k)
+{
+  list( trn.set = rep(0.5,k),tst.set = rep(0.5,k))
+}
+
+best.s.feature <- features[300]
 ######################
 # SAMPLE
 
